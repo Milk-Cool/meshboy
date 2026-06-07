0e433f426f8dec99273d0fa12037852be5c29625
Milk-Cool <coder.tomsk@gmail.com>
2026-06-07 17:28:29 +0300

    add support for wram

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ uint8_t uart_rx_buf[0x100];
 uint8_t uart_tx_buf[0x100];
 uint8_t uart_n = 0;
 
-uint8_t ram[0x2000]; // 8KB; could have gotten away with just having the above variable in this one... but it's whatever really
+uint8_t ram[0x4000]; // 16KB; could have gotten away with just having the above variable in this one... but it's whatever really
 
 void setup() {
     // we don't care about the time taken to set everything up
@@ -62,7 +62,7 @@ void loop() {
         if(addr >= 0xa200 && addr < 0xa300) uart_tx_buf[addr - 0xa200] = data; // set data to write to esp32
         else if(addr == 0xa300) uart_n = Serial.readBytes(uart_rx_buf, data); // read data from esp32
         else if(addr == 0xa301) Serial.write(uart_tx_buf, data); // send data to esp32
-        else if(addr >= 0xa000 && addr < 0xc000) ram[addr - 0xa000] = data; // writing to ram
+        else if(addr >= 0xa000 && addr < 0xe000) ram[addr - 0xa000] = data; // writing to ram
     } else {
         // read operation
         sio_hw->gpio_oe_set |= GB_D;
@@ -71,7 +71,7 @@ void loop() {
         if(addr >= 0xa100 && addr < 0xa200) data = uart_rx_buf[addr - 0xa100]; // reading from read data buffer
         else if(addr == 0xa302) data = uart_n; // read data byte count
         else if(addr >= 0x0000 && addr < 0x4000) data = rom[addr]; // reading from rom
-        else if(addr >= 0xa000 && addr < 0xc000) data = ram[addr - 0xa000]; // reading from ram
+        else if(addr >= 0xa000 && addr < 0xe000) data = ram[addr - 0xa000]; // reading from ram
         else data = 0; // fallback
         // (sio_hw->gpio_in & (P(GB_D_MAX + 1) - 1)) >> GB_D_MIN;
         sio_hw->gpio_out &= (~(P(GB_D_MAX + 1) - 1)) ^ (~(P(GB_D_MIN + 1) - 1)); // an even more convoluted way of clearing the state of all the unneeded pins
